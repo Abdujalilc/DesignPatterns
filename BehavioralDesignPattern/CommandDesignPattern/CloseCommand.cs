9a94a157d0a7852642c39dc8b85f474aef556ff6
Abdujalilc <abdujalilc@gmail.com>
2022-03-03 09:37:10 +0500

    Command DP added

---
+++ BehavioralDesignPattern/CommandDesignPattern/CloseCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace CommandDesignPattern
+{
+    class CloseCommand : ICommand
+    {
+        private Document document;
+        public CloseCommand(Document doc)
+        {
+            document = doc;
+        }
+        public void Execute()
+        {
+            document.Close();
+        }
+    }
+}
---
+++ BehavioralDesignPattern/CommandDesignPattern/Document.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CommandDesignPattern
+{
+    public class Document
+    {
+        public void Open()
+        {
+            Console.WriteLine("Document Opened");
+        }
+        public void Save()
+        {
+            Console.WriteLine("Document Saved");
+        }
+        public void Close()
+        {
+            Console.WriteLine("Document Closed");
+        }
+    }
+}
