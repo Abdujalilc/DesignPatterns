a8057833fbcd6468ccda67d1e66328fb14a4d768
Abdujalilc <abdujalilc@gmail.com>
2022-03-03 16:42:15 +0500

    some changes

--- BehavioralDesignPattern/MediatorDesignPattern/Program.cs
+++ BehavioralDesignPattern/MediatorDesignPattern/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MediatorDesignPattern;
 
 FacebookGroupMediator facebookMediator = new ConcreteFacebookGroupMediator();
+
 User Ram = new ConcreteUser(facebookMediator, "Ram");
 User Dave = new ConcreteUser(facebookMediator, "Dave");
 User Smith = new ConcreteUser(facebookMediator, "Smith");
@@ -9,6 +10,7 @@ User Sam = new ConcreteUser(facebookMediator, "Sam");
 User Pam = new ConcreteUser(facebookMediator, "Pam");
 User Anurag = new ConcreteUser(facebookMediator, "Anurag");
 User John = new ConcreteUser(facebookMediator, "John");
+
 facebookMediator.RegisterUser(Ram);
 facebookMediator.RegisterUser(Dave);
 facebookMediator.RegisterUser(Smith);
@@ -17,6 +19,7 @@ facebookMediator.RegisterUser(Sam);
 facebookMediator.RegisterUser(Pam);
 facebookMediator.RegisterUser(Anurag);
 facebookMediator.RegisterUser(John);
+
 Dave.Send("dotnettutorials.net - this website is very good to learn Design Pattern");
 Console.WriteLine();
 Rajesh.Send("What is Design Patterns? Please explain ");
